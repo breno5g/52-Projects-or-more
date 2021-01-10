58ba06142e62f14b0b60c29ee1974ed24983ae3f
Breno Santos <brenosantos145@gmail.com>
2021-01-10 20:28:49 -0300

    v2

---
+++ Project 02 - Contact Page/Page v2/script.js
@@ -0,0 +1,10 @@
+function openNewWindow(tag) {
+    if (tag == "github") {
+        window.open("http://www.github.com/breno5g")
+    } else if (tag == "insta") {
+        window.open("https://www.instagram.com/breno.index/") 
+    
+    } else {
+        window.open("https://www.linkedin.com/in/breno-santos-80748614a/")
+    }
+}
