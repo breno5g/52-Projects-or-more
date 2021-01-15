1dfb76bf36ed971463e3ae761aad95cc17f6d15b
Breno Santos <51424478+breno5g@users.noreply.github.com>
2021-01-15 20:21:04 -0300

    Update script.js

--- Project 02 - Contact Page/Page v2/script.js
+++ Project 02 - Contact Page/Page v2/script.js
@@ -1,10 +1,10 @@
 function openNewWindow(tag) {
     if (tag == "github") {
-        window.open("http://www.github.com/breno5g")
+        window.open("http://www.github.com/breno5g");
     } else if (tag == "insta") {
-        window.open("https://www.instagram.com/breno.index/") 
+        window.open("https://www.instagram.com/breno.index/");
     
     } else {
-        window.open("https://www.linkedin.com/in/breno-santos-80748614a/")
+        window.open("https://www.linkedin.com/in/breno-santos-80748614a/");
     }
 }
