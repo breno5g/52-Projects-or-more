2498c65d4cb6897ac28dfd65d32e95fd5c37e1b9
breno5g <brenosantos145@gmail.com>
2021-04-04 14:17:11 -0300

    muitos updates

--- Project 14 - Crowdfunding page/script.js
+++ Project 14 - Crowdfunding page/script.js
@@ -1,18 +1,26 @@
+let initialRadio;
+let actualRadio;
+let allRadio = document.querySelectorAll(`[type="radio"]`);
+let resetRadio;
+
 function modal() {
-    let radio = document.querySelector(`[type="radio"]:checked`);
-    let box = radio.parentElement.parentElement.parentElement;
-    box.style.boxShadow = "0 0 0 3px #3cb4ac"
+    initialRadio = document.querySelector(`[type="radio"]:checked`);
+    let box = initialRadio.parentElement.parentElement.parentElement;
+    box.style.boxShadow = "0 0 0 3px #3cb4ac";
+    box.children[1].style.display = "flex";
 }
 
 function showPledge(e) {
-    let main = e.parentElement.parentElement.parentElement;
-    let radio = document.querySelectorAll(`[type="radio"]`);
-    for(let i = 0; i < radio.length; i++) {
-        if(radio[i].checked) {
-            main.style.boxShadow = "0 0 0 3px #3cb4ac"
-        } else {
-            let c = radio[i].parentElement.parentElement.parentElement;
-            c.style.boxShadow = "none"
+    actualRadio = e.parentElement.parentElement.parentElement;
+    allRadio.forEach((e) => {
+        resetRadio = e.parentElement.parentElement.parentElement
+        resetRadio.style.boxShadow = "0 0 2px 0 #00000042";
+        if(resetRadio.children[1]) {
+            resetRadio.children[1].style.display = "none";
         }
-    }
+    });
+    if(actualRadio.children[1]) {
+        actualRadio.style.boxShadow = "0 0 0 3px #3cb4ac"
+        actualRadio.children[1].style.display = "flex";
+    }   
 }
