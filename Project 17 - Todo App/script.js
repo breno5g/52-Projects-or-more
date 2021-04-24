e93a43ca96869d89555f8a068b0e510580c7d88c
breno5g <brenosantos145@gmail.com>
2021-04-24 14:29:32 -0300

    Clear completed function

--- Project 17 - Todo App/script.js
+++ Project 17 - Todo App/script.js
@@ -2,6 +2,10 @@ let todoList = [
   {id: 0, text: "primeiro"},
   {id: 1, text: "segundo"},
   {id: 2, text: "terceiro"},
+  {id: 4, text: "Sem Paciencia mermão"},
+  {id: 5, text: "Sem Paciencia mermão"},
+  {id: 6, text: "Sem Paciencia mermão"},
+  {id: 7, text: "Sem Paciencia mermão"},
 ];
 
 let todoContainer = document.querySelector(".todos");
@@ -18,11 +22,10 @@ function createNewTodo(e) {
 }
 
 function createElements() {
-  console.log(todoContainer)
   for(let i = todoList.length - 1; i >= 0; i--) {
     let todo = document.createElement("div");
     todo.classList.add("todo");
-    // todo.setAttribute("id", `${todoList[i].id}`)
+    todo.setAttribute("id", `${todoList[i].id}`)
     let leftSide = document.createElement("div");
     leftSide.classList.add("left-side");
     let checkbox = document.createElement("input");
@@ -48,22 +51,42 @@ function clearList() {
   todoContainer.innerHTML = null;
 }
 
+function clearCompleted() {
+  let checkbox = document.querySelectorAll(`[type = "checkbox"]`);
+  for(let i = 0; i < checkbox.length; i++) {
+    let todo = checkbox[i].parentElement.parentElement;
+    if (checkbox[i].checked) {
+      todoList.splice(todo.getAttribute("id"), 1);
+      loadTodo();
+    }
+  }
+}
+
 
 function loadTodo() {
   clearList();
   createElements();
+  itemsLeft();
 }
 
 function check(e) {
-    let checkbox = e.parentElement.children[0];
-    let text = e.parentElement.parentElement.children[1].children[0]
-    if (checkbox.checked == true) {
-      checkbox.checked = false;
-      text.style.textDecoration = "none";
-      text.style.color = "#cacde8";
-    } else {
-      checkbox.checked = true;
-      text.style.color = "#4d5066";
-      text.style.textDecoration = "line-through";
-    }
+  let checkbox = e.parentElement.children[0];
+  let text = e.parentElement.parentElement.children[1].children[0]
+  if (checkbox.checked == true) {
+    checkbox.checked = false;
+    text.style.textDecoration = "none";
+    text.style.color = "#cacde8";
+  } else {
+    checkbox.checked = true;
+    text.style.color = "#4d5066";
+    text.style.textDecoration = "line-through";
+  }
 }
+
+function itemsLeft() {
+  let span = document.querySelector(".counter").children[0];
+  span.innerText = `${todoList.length} items left`
+  
+}
+
+window.addEventListener("load", loadTodo());
